32a4b1aaa62097121f3cdb9bd3b316a307861490
GopalDhorajiya <gopaldhorajiya205@gmail.com>
2026-01-08 18:18:57 +0530

    Student Answer photo Creation Api generation is done with photo upload in Cloudanary

--- Auto-Grade-Springboot/src/main/java/com/ogs/autograde/models/StudentQuesAns.java
+++ Auto-Grade-Springboot/src/main/java/com/ogs/autograde/models/StudentQuesAns.java
@@ -1,6 +1,8 @@
 package com.ogs.autograde.models;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import lombok.*;
@@ -21,20 +23,33 @@ public class StudentQuesAns extends BaseModel{
 
     private float accuracy_cmp;
 
-    @Column(nullable = false)
+    @Column(nullable = true)
     private String question;
 
     @OneToOne(cascade=CascadeType.ALL)
     @JoinColumn(name = "image_id")
     private Image photo;
 
-    @ManyToOne()
+    @ManyToOne
     @JoinColumn(name = "sqa_stu_id")
+    @JsonIgnore
     private Student student;
 
     @ManyToOne
     @JoinColumn(name = "sqa_fqa_id")
+    @JsonIgnore
     private FacultyQuesAns facultyQuesAns;
 
+    @JsonProperty("studentId")
+    public Long getStudentId()
+    {
+        return student != null ? student.getId() : null;
+    }
+
+    @JsonProperty("questionId")
+    public Long getQuestionId()
+    {
+        return facultyQuesAns != null ? facultyQuesAns.getId() : null;
+    }
 
 }
--- Auto-Grade-Springboot/src/main/java/com/ogs/autograde/services/StudentServicesImp.java
+++ Auto-Grade-Springboot/src/main/java/com/ogs/autograde/services/StudentServicesImp.java
@@ -1,7 +1,23 @@
 package com.ogs.autograde.services;
 
+import com.ogs.autograde.Repository.StudentRepo;
+import com.ogs.autograde.models.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentServicesImp implements IStudentServices{
+
+    final private StudentRepo studentRepo;
+
+    public StudentServicesImp(@Autowired StudentRepo studentRepo) {
+        this.studentRepo = studentRepo;
+    }
+
+    @Override
+    public Student createStudent(String name) {
+        System.out.println(name);
+        Student student = Student.builder().name(name).build();
+        return studentRepo.save(student);
+    }
 }
