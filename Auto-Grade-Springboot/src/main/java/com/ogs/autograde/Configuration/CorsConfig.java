1f7b0e4fcfb9d6d68c7c3cf5ed677bf2bcae1bb9
Bhargav-611 <161122294+Bhargav-611@users.noreply.github.com>
2026-01-19 15:15:20 +0530

    change package name

--- Auto-Grade-Springboot/src/main/java/com/ogs/autograde/Configuration/CorsConfig.java
+++ Auto-Grade-Springboot/src/main/java/com/ogs/autograde/Configuration/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.yourproject.config;
+package com.ogs.autograde.Configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
